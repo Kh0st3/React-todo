65efd42cd30f8eea14bb33e04a8a3b76769519ba
Ricardo <ricardocc5@hotmail.com>
2023-11-23 22:00:39 -0500

    Segunda sesion

--- src/App.jsx
+++ src/App.jsx
@@ -4,104 +4,106 @@ import MoonIcon from "./components/icons/MoonIcon";
 const App = () => {
     return (
         <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen">
-          <header className="container mx-auto px-4 pt-8">
-            <div className="flex justify-between"> 
-              <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
-              <button>
-                <MoonIcon className="fill-red-400"/>
-              </button>
-            </div>
-            <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4 mt-8">
-              <span className="rounded-full border-2 w-5 h-5 inline-block"></span>
-              <input 
-                type="text" 
-                placeholder="Create a new todo..."
-                className="w-full text-gray-400 outline-none"
-                />
-            </form>
-          </header>
+            {/* Header */}
+            <header className="container mx-auto px-4 pt-8">
+                <div className="flex justify-between">
+                    <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">
+                        Todo
+                    </h1>
+                    <button>
+                        <MoonIcon className="fill-red-400" />
+                    </button>
+                </div>
+            </header>
 
-          <main className="container mx-auto px-4 mt-8">
-            <div className="bg-white rounded-md [&>article]:p-4">
+            <main className="container mx-auto px-4 mt-8">
+                {/* TodoCreate */}
+                <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4">
+                    <span className="rounded-full border-2 w-5 h-5 inline-block"></span>
+                    <input
+                        type="text"
+                        placeholder="Create a new todo..."
+                        className="w-full text-gray-400 outline-none"
+                    />
+                </form>
 
-              <article className="flex gap-4 border-b-gray-400 border-b">
-                <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
-                <p className="text-gray-600 grow">
-                  Complete online Javascript curse in RamMedia
-                </p>
-                <button className="flex-none">
-                  <CrossIcon />
-                </button>
-              </article>
-              <article className="flex gap-4 border-b-gray-400 border-b">
-                <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
-                <p className="text-gray-600 grow">
-                  Jog around the jpark 3x
-                </p>
-                <button className="flex-none">
-                  <CrossIcon />
-                </button>
-              </article>
-              <article className="flex gap-4 border-b-gray-400 border-b">
-                <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
-                <p className="text-gray-600 grow">
-                  10 minutes meditation
-                </p>
-                <button className="flex-none">
-                  <CrossIcon />
-                </button>
-              </article>
-              <article className="flex gap-4 border-b-gray-400 border-b">
-                <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
-                <p className="text-gray-600 grow">
-                  Read for 1 hour
-                </p>
-                <button className="flex-none">
-                  <CrossIcon />
-                </button>
-              </article>
-              <article className="flex gap-4 border-b-gray-400 border-b">
-                <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
-                <p className="text-gray-600 grow">
-                  Pick up groceries
-                </p>
-                <button className="flex-none">
-                  <CrossIcon />
-                </button>
-              </article>
-              <article className="flex gap-4 border-b-gray-400 border-b px-4">
-                <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
-                <p className="text-gray-600 grow">
-                  Complete Todo App on Frontend Mentor
-                </p>
-                <button className="flex-none">
-                  <CrossIcon />
-                </button>
-              </article>
-              
-              <section className="py-4 px-4 flex justify-between">
-                <span className="text-gray-400">5 items left</span>
-                <button className="text-gray-400">
-                  Clear Completed
-                </button>
-              </section>
-            </div>
-          </main>
+                {/* TodoList (TodoItem) TodoUpdate & TodoDelete */}
+                <div className="bg-white rounded-t-md [&>article]:p-4 mt-8">
+                    <article className="flex gap-4 border-b-gray-400 border-b">
+                        <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                        <p className="text-gray-600 grow">
+                            Complete online Javascript curse in RamMedia
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+                    <article className="flex gap-4 border-b-gray-400 border-b">
+                        <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                        <p className="text-gray-600 grow">
+                            Jog around the jpark 3x
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+                    <article className="flex gap-4 border-b-gray-400 border-b">
+                        <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                        <p className="text-gray-600 grow">
+                            10 minutes meditation
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+                    <article className="flex gap-4 border-b-gray-400 border-b">
+                        <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                        <p className="text-gray-600 grow">Read for 1 hour</p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+                    <article className="flex gap-4 border-b-gray-400 border-b">
+                        <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                        <p className="text-gray-600 grow">Pick up groceries</p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+                    <article className="flex gap-4 border-b-gray-400 border-b px-4">
+                        <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                        <p className="text-gray-600 grow">
+                            Complete Todo App on Frontend Mentor
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+                </div>
 
-          <section className="px-4 container mx-auto mt-8">
-            <div className="bg-white p-4 rounded-md flex justify-center gap-4">
+                {/* TodoComputed */}
+                <section className="py-4 px-4 flex justify-between bg-white rounded-b-md">
+                    <span className="text-gray-400">5 items left</span>
+                    <button className="text-gray-400">Clear Completed</button>
+                </section>
 
-              <button className="text-blue-600">All</button>
-              <button className="hover:text-blue-600">Active</button>
-              <button className="hover:text-blue-600">Completed</button>
-              
-            </div>
-          </section>
+                {/* TodoFilter */}
+                <section className="container mx-auto mt-8">
+                    <div className="bg-white p-4 rounded-md flex justify-center gap-4">
+                        <button className="text-blue-600">All</button>
+                        <button className="hover:text-blue-600">Active</button>
+                        <button className="hover:text-blue-600">
+                            Completed
+                        </button>
+                    </div>
+                </section>
+            </main>
 
-          <p className="text-center mt-8">Drag and Drop to reorder list</p>
-        
+            <footer className="text-center mt-8">
+                Drag and Drop to reorder list
+            </footer>
         </div>
-      );
+    );
 };
 
 export default App;
